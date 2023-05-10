5ca1c36c32459e8352d1b46b30b8cf2ab70e03c9
Simo Hamed <122347903+RakanGarvey@users.noreply.github.com>
2023-05-10 02:53:29 +0100

    Update Rent.jsx

--- src/components/Rent.jsx
+++ src/components/Rent.jsx
@@ -6,36 +6,15 @@ import {IoMdPricetag} from 'react-icons/io'
 import { useState } from 'react'
 
 const Rent = () => {
-    const [visiblity,setVisiblity] = useState(false)
-    const [showRent,setShowRent] = useState(false)
     const [showPrice,setshowPrice] = useState(false)
-    const changeState = ()=> {
-        setVisiblity(!visiblity)
-    }
-    const show1 = ()=> {
-        setShowRent(!showRent)
-    }
     const show2 = ()=> {
         setshowPrice(!showPrice)
     }
-
-    const {prop,setProp} = useState("georgia")
-    let text = `Location`
-    let val
-    const changeProp = (event) => {
-        // setProp(event.target.value);
-        text = event.target.val
-      };
-      const changeText = (event) => {
-        // setProp(event.target.value);
-        text = event.target.val
-      };
   return (
     <div className='rent'>
         <div className="rent-details">
             <div className="location det details">
                 <HiLocationMarker className='icons'/>
-                {/* <button className='details-btn rent-btn' onClick={show1}>{text} <IoIosArrowDown className='icon'/></button> */}
                 <select className='details-btn rent-btn'>
                   <option value="1">Location</option>
                   <option value="2">Georgia</option>
@@ -43,7 +22,6 @@ const Rent = () => {
             </div>
             <div className="prop-type det details">
                 <HiHome className='icons'/>
-                {/* <button className='details-btn rent-btn' onClick={changeState}>{text}</button> */}
                 <select className='details-btn rent-btn' >
                   <option value="1">Property Type</option>
                   <option value="2">Standard Room</option>
@@ -58,36 +36,6 @@ const Rent = () => {
             <button className="search-btn">Search</button>
         </div>
         <div className="rent-input">
-        {/* {showRent && (
-          <label>
-            <input type='text' className='loca-input input' value={val} onChange={changeText}/>
-          </label>
-        )} */}
-        {/* {visiblity && (
-          <label className='label'>
-            <div
-              className='prop-input'
-              data-value='Apartment'
-              onClick={changeProp}
-            >
-              Standard Room
-            </div>
-            <div
-              className='prop-input'
-              data-value='House'
-              onClick={changeProp}
-            >
-              Deluxe Room
-            </div>
-            <div
-              className='prop-input'
-              data-value='Villa'
-              onClick={changeProp}
-            >
-              Superior Room
-            </div>
-          </label>
-        )} */}
         {showPrice && (
           <label>
             <input type='number' className='price-input input' />

42b55e95b9ed60774526d25c2c817fbc58934636
Simo Hamed <122347903+RakanGarvey@users.noreply.github.com>
2023-05-10 02:59:23 +0100

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import {BsStarFill} from 'react-icons/bs'
-import {GrFormNext} from 'react-icons/gr'
-import {GrFormPrevious} from 'react-icons/gr'
-import { useState } from 'react'
 
 const Reviews = () => {
     
